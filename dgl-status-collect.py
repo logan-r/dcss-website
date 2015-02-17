38ab7989ed2e837b4bd3c4e53827773ca9c71acf
Alex Jurkiewicz <alex@jurkiewi.cz>
2015-02-17 21:00:00 +1100

    add milestone data to dgl-status, and improve general error handling

--- dgl-status-collect.py
+++ dgl-status-collect.py
@@ -11,6 +11,26 @@ import urllib2
 SP_TABLE = {'Mf': 'Merfolk', 'Ke': 'Kenku*', 'MD': 'Mountain Dwarf*', 'Og': 'Ogre', 'Na': 'Naga', 'DD': 'Deep Dwarf', 'DE': 'Deep Elf', 'Tr': 'Troll', 'Mu': 'Mummy', 'GE': 'Grey Elf*', 'VS': 'Vine Stalker', 'HO': 'Hill Orc', 'Sp': 'Spriggan', 'Te': 'Tengu', 'HD': 'Hill Dwarf*', 'HE': 'High Elf', 'El': 'Elf*', 'OM': 'Ogre-Mage*', 'Dj': 'Djinni*', 'Gr': 'Gargoyle', 'Ko': 'Kobold', 'Dg': 'Demigod', 'Gh': 'Ghoul', 'Fo': 'Formicid', 'Ce': 'Centaur', 'Hu': 'Human', 'Vp': 'Vampire', 'Op': 'Octopode', 'Mi': 'Minotaur', 'Pl': 'Plutonian*', 'LO': 'Lava Orc*', 'Gn': 'Gnome*', 'Ha': 'Halfling', 'Dr': 'Draconian', 'Ds': 'Demonspawn', 'SE': 'Sludge Elf*', 'Fe': 'Felid'}
 BG_TABLE = {'Pr': 'Priest*', 'CK': 'Chaos Knight', 'AE': 'Air Elementalist', 'DK': 'Death Knight', 'Cj': 'Conjurer', 'EE': 'Earth Elementalist', 'Mo': 'Monk', 'AM': 'Arcane Marksman', 'Ne': 'Necromancer', 'Su': 'Summoner', 'VM': 'Venom Mage', 'Sk': 'Skald', 'Re': 'Reaver*', 'Pa': 'Paladin*', 'FE': 'Fire Elementalist', 'Th': 'Thief*', 'Cr': 'Crusader*', 'St': 'Stalker*', 'IE': 'Ice Elementalist', 'Be': 'Berserker', 'En': 'Enchanter', 'Wn': 'Wanderer', 'Jr': 'Jester*', 'Hu': 'Hunter', 'AK': 'Abyssal Knight', 'As': 'Assassin', 'Ar': 'Artificer', 'Wr': 'Warper', 'Fi': 'Fighter', 'Gl': 'Gladiator', 'Tm': 'Transmuter', 'Wz': 'Wizard', 'He': 'Healer'}
 BR_TABLE = {'D': 'Dungeon', 'Orc': 'Orcish Mines', 'Elf': 'Elven Halls', 'Lair': 'Lair of the Beasts', 'Depths': 'Depths', 'Swamp': 'Swamp', 'Shoals': 'Shoals', 'Spider': 'Spider Nest', 'Snake': 'Snake Pit', 'Slime': 'Slime Pits', 'Vaults': 'Vaults', 'Crypt': 'Crypt', 'Tomb': 'Tomb of the Ancients', 'Dis': 'Iron City of Dis', 'Geh': 'Gehenna', 'Coc': 'Cocytus', 'Tar': 'Tartarus', 'Zot':'Realm of Zot', 'Abyss': 'Abyss', 'Zig': 'Ziggurat', 'Lab': 'Labyrinth', 'Bazaar': 'Bazaar', 'WizLab': 'Wizard\'s Laboratory', 'Sewer': 'Sewer', 'Bailey': 'Bailey', 'Ossuary': 'Ossuary', 'IceCv': 'Ice Cave', 'Volcano': 'Volcano', 'Hell': 'Vestibule of Hell', 'Temple': 'Ecumenical Temple', 'Pan': 'Pandemonium', 'Trove': 'Treasure Trove'}
+MILESTONE_URL = 'https://loom.shalott.org/api/sequell/game?q=!lm+{nick}&count=1'
+
+def get_milestone(nick):
+    url = MILESTONE_URL.format(nick=nick)
+    try:
+        response = urllib2.urlopen(url, timeout=5)
+    except urllib2.URLError:
+        print "Warning: couldn't grab %s" % url
+        return None
+    if response.getcode() != 200:
+        print "Warning: %s returned status code %s, skipping." % (url, response.getcode())
+        return None
+    data = response.read()
+    try:
+        milestone = json.loads(data)["records"][0]
+    except StandardError:
+        print "Warning: couldn't parse milestone for %s, skipping." % nick
+        print "Bad milestone was: %s" % data
+        milestone = None
+    return milestone
 
 def parse_location(location):
     """Parse raw location string and return (branch, branchlevel, humanreadable).
@@ -99,6 +119,7 @@ def get_games(servers):
             continue
         if response.getcode() != 200:
             print "Warning: %s returned status code %s, skipping." % (url, response.getcode())
+            continue
         for line in response.read().splitlines():
             try:
                 game = parse_line(line)
@@ -111,6 +132,7 @@ def get_games(servers):
             game['source'] = server['shortname']
             if 'watchurl' in server:
                 game['watchurl'] = server['watchurl'].format(name=game['name'])
+            game['latest_milestone'] = get_milestone(game['name'])
             games.append(game)
 
     return games
@@ -129,12 +151,13 @@ if __name__ == '__main__':
     OUTFILE = sys.argv[2]
     try:
         SERVERS = json.load(open(sys.argv[1], 'r'))
-    except Exception:
+    except StandardError:
         print "Error: couldn't load %s!" % SERVERS
+        print traceback.format_exc()
         sys.exit(1)
 
     try:
         dump_games(get_games(SERVERS), OUTFILE)
     except StandardError as e:
-        print "Error: unhandled exception: %s.\nFull traceback:\n%s" % (e, traceback.format_exc())
+        print "Error: unhandled exception::\n%s" % traceback.format_exc()
         sys.exit(1)
